79d92ae9e8c92954c2297a7ecb97e95d44fdeec0
Surya Sunkari <suryasunkari@gmail.com>
2023-08-04 20:49:08 -0500

    modal checkpoint

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -4,7 +4,6 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
 
 
 
@@ -31,19 +30,22 @@ const PortfolioCard = ({portfolios, id, src, name}) => {
                 open={open}
                 onClose={handleClose}
                 closeAfterTransition
+                style={{overflow: 'scroll'}}
             >
-                <Fade in={open}>
-                    <Box className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-7/12 h-3/5 
-                                    flex  items-center flex-col bg-gray-800 rounded-md'>
-                        <h1 className=' text-6xl text-center text-white font-bold p-8 m-8 pb-4 mb-4'>{modalInfo.name}</h1>
-                        <p className=' text-2xl text-center text-white font-normal p-8'>{modalInfo.desc}</p>
-                        <Stack direction="row" spacing={12}>
-                            {modalInfo.demo!==undefined && <Button href={modalInfo.demo} variant="contained">Demo</Button>}
-                            {modalInfo.code!==undefined && <Button href={modalInfo.code} variant="contained">Code</Button>}
-                            {modalInfo.paper!==undefined && <Button href={modalInfo.paper} variant="contained">Paper</Button>}
-                        </Stack>
-                    </Box>
-                </Fade>
+                <div className=''>
+                    <Fade in={open}>
+                        <Box className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-7/12 h-3/5 
+                                        flex  items-center flex-col bg-gray-800 rounded-md'>
+                            <h1 className=' text-6xl text-center text-white font-bold p-6 m-6 pb-0 mb-0'>{modalInfo.name}</h1>
+                            <p className=' text-2xl text-center text-white font-normal p-8'>{modalInfo.desc}</p>
+                            <Stack direction="row" spacing={12}>
+                                {modalInfo.demo!==undefined && <Button href={modalInfo.demo} variant="contained">Demo</Button>}
+                                {modalInfo.code!==undefined && <Button href={modalInfo.code} variant="contained">Code</Button>}
+                                {modalInfo.paper!==undefined && <Button href={modalInfo.paper} variant="contained">Paper</Button>}
+                            </Stack>
+                        </Box>
+                    </Fade>
+                </div>
             </Modal>
         </>
      );
